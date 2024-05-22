06180569bd534d961e4c274359395ecab9427958
DadoveNonye <98288131+DadoveNonye@users.noreply.github.com>
2024-05-22 23:22:50 +0100

    Add necessary files and configurations for React app

---
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <header className="App-header">
+        <img src={logo}></img>
+        <h1>School dashboard</h1>
+      </header>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <footer className="App-footer">
+        <p>Copyright 2020 - holberton School</p>
+      </footer>
+    </>
+  );
+};
+
+export default App;
