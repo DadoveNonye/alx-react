8fc75cdf6bbe5eda8b7d40e49be8642ff25ec346
DadoveNonye <98288131+DadoveNonye@users.noreply.github.com>
2024-05-26 23:45:23 +0100

    chore: Add Footer and Header components with CSS styles

---
+++ 0x02-react_props/task_0/dashboard/src/Login/Login.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Login.css";
+
+function Login() {
+  return (
+    <>
+      <div>
+        <p>Login to access the full dashboard</p>
+      </div>
+
+      <form>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" name="email" />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" />
+        </div>
+        <button type="button" onClick={() => console.log("OK button clicked")}>
+          OK
+        </button>
+      </form>
+    </>
+  );
+}
+
+export default Login;
