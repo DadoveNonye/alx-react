0e04bd01d102bed8bed4c291bc97e1e4f6b9c656
DadoveNonye <98288131+DadoveNonye@users.noreply.github.com>
2024-05-30 05:44:05 +0100

    chore: Update import paths for components in App.js

---
+++ 0x03-React_component/README.md
@@ -0,0 +1,12 @@
+0x03-React_component
+Resources
+Read or watch:
+
+React components
+React Developer Tools
+Enzyme Shallow
+Enzyme Mount
+Enzyme Unmount
+React Pure components
+React Higher Order Components
+Jest mock function
---
+++ 0x03-React_component/task_1/dashboard/src/App/App.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Header from "../Header/Header";
+import Login from "../Login/Login";
+import Footer from "../Footer/Footer";
+import Notifications from "../../../../task_2/dashboard/src/Notifications/Notifications";
+import CourseList from "../CourseList/CourseList";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleKeyDown);
+  }
+
+  handleKeyDown(event) {
+    if (event.ctrlKey && event.key === "h") {
+      alert("Logging you out");
+      this.props.logOut();
+    }
+  }
+
+  render() {
+    const { isLoggedIn } = this.props;
+    const listCourses = [
+      { id: 1, name: "ES6", credit: 60 },
+      { id: 2, name: "Webpack", credit: 20 },
+      { id: 3, name: "React", credit: 40 },
+    ];
+
+    return (
+      <>
+        <Notifications />
+        <div className="App">
+          <Header />
+          <div className="App-body">
+            {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
+          </div>
+          <Footer />
+        </div>
+      </>
+    );
+  }
+}
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+  logOut: PropTypes.func,
+};
+
+App.defaultProps = {
+  isLoggedIn: false,
+  logOut: () => {},
+};
+
+export default App;
