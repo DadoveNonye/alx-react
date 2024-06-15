affc13df276d47c75ba1095da17b3d338231461c
DadoveNonye <98288131+DadoveNonye@users.noreply.github.com>
2024-06-15 06:39:51 +0100

    feat: Update 0x06-React_Immutable/4-mutations.js to use Immutable.js for creating and modifying maps

---
+++ 0x06-React_Immutable/4-mutations.js
@@ -0,0 +1,14 @@
+import { Map } from "immutable";
+
+// Create the initial Immutable Map
+export const map = Map({
+  1: "Liam",
+  2: "Noah",
+  3: "Elijah",
+  4: "Oliver",
+  5: "Jacob",
+  6: "Lucas",
+});
+
+// Create the second Immutable Map by modifying the initial Map
+export const map2 = map.set(2, "Benjamin").set(4, "Oliver");
