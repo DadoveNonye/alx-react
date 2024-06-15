29a7a647dc86b789acf36997291fb1930d794f6f
DadoveNonye <98288131+DadoveNonye@users.noreply.github.com>
2024-06-15 07:13:52 +0100

    refactor: Remove unused import and update mergeDeeplyElements function to use Immutable.js for merging objects

--- 0x06-React_Immutable/6-deeply.js
+++ 0x06-React_Immutable/6-deeply.js
@@ -1,7 +1,7 @@
-import { List, Map } from "immutable";
+import { Map } from "immutable";
 
 // Function to deeply merge two objects and return a List containing the values
-export function mergeDeeplyElements(page1, page2) {
+export default function mergeDeeplyElements(page1, page2) {
   // Convert objects to Immutable Maps
   const map1 = Map(page1);
   const map2 = Map(page2);
